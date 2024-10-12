cb16652ddef12a69cddaa631373d75524afa85e7
razborovdb <razborovdb@gmail.com>
2024-10-12 09:18:15 +0500

    magic number

---
+++ src/main/java/learn01/training/interviewquestions/magicnumber/MagicNumber.java
@@ -0,0 +1,21 @@
+package learn01.training.interviewquestions.magicnumber;
+
+public class MagicNumber {
+    public static void main(String[] args) {
+        System.out.println(isMagic(163));
+    }
+
+    public static boolean isMagic(int n) {
+        int cur = n;
+        int sum = 0;
+        while (cur > 0 || sum > 9) {
+            if (cur == 0) {
+                cur = sum;
+                sum = 0;
+            }
+            sum += cur % 10;
+            cur  /= 10;
+        }
+        return sum == 1;
+    }
+}
